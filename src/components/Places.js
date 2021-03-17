04a3e36e7a888b9dff917da238588ed4c3a41c99
arjav <arjav@umich.edu>
2021-03-17 00:21:48 -0400

    create table

--- src/components/Places.js
+++ src/components/Places.js
@@ -2,7 +2,35 @@ import React from 'react';
 import './Places.css';
 
 function Places(props) { 
-    return;
+    return(
+        <div className = "places">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Departure Code</th>
+                        <th>Departure</th>
+                        <th>Destination Code</th>
+                        <th>Destination</th>
+                        <th>Cost</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {props.places.map(place => {
+                        return (
+                            <tr id={place.PlaceId}> 
+                                <th>{place.PlaceId}</th>
+                                <th>{place.PlaceName}</th>
+                                <th>{place.DestId}</th>
+                                <th>{place.DestName}</th>
+                                <th>(place.Price)</th>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default Places;
